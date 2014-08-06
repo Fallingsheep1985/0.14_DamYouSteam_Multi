8e15a28b76b75a14fba3563ec6cc6b11af0cc5d9
fallingsheep <razorsoftinfo@gmail.com>
2014-08-06 15:51:54 +1000

    FIX DEFINES

--- MPMissions/DayZ_Epoch_11.Chernarus/defines.hpp
+++ MPMissions/DayZ_Epoch_11.Chernarus/defines.hpp
@@ -510,13 +510,3 @@ class RscBackground
 	sizeEx = 0.02;
 	text = "";
 };
-class RscPicture 
-{
-	type = CT_STATIC;
-	idc = -1;
-	style = ST_PICTURE;
-	colorBackground[] = {0, 0, 0, 0};
-	colorText[] = {1, 1, 1, 1};
-	font = "Bitstream"; //Typeface
-	sizeEx = 0;
-};
